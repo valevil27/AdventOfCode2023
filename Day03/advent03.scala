074bbe21731bad409fcff14de68bb65d0f22d6dc
valero <evalerovilella@gmail.com>
2023-12-03 21:41:26 +0100

    Solved Day03 second star

--- Day03/advent03.scala
+++ Day03/advent03.scala
@@ -2,37 +2,53 @@ package com.valevil27.advent2023.Day03
 
 import scala.io.Source
 import scala.util.boundary
+import math.{max, min}
+import scala.collection.mutable.ArrayBuffer
 
 object Star05:
-  class Symbol(var symbol: Char = ' ', var row: Int = 0, var col: Int = 0)
+  class Symbol(var symbol: Char = ' ', var row: Int = 0, var col: Int = 0):
+    var Numbers = List[Number]()
+    override def toString(): String = s"$symbol"
+    override def equals(x: Any): Boolean = x match
+      case s: Symbol => s.symbol == symbol && s.col == col && s.row == row
+      case _         => false
+
   class Number(
       val value: Int,
       val row: Int,
       val startIndex: Int,
       val endIndex: Int
   ) {
-    var symbol = Symbol()
-    def hasSurround(lines: Seq[String]): Boolean =
-      val fromRow = if row > 0 then row - 1 else row
-      val toRow = if row < lines.length - 1 then row + 1 else row
-      val fromIndex = if startIndex > 0 then startIndex - 1 else startIndex
-      val toIndex =
-        if endIndex < lines(0).length - 1 then endIndex + 1 else endIndex
-      var has = false
-      boundary:
-        for r <- fromRow to toRow; i <- fromIndex to toIndex do
-          lines(r)(i) match
-            case c if c.isDigit => {}
-            case c if c == '.'  => {}
-            case c: Char =>
-              has = true
-              symbol = Symbol(c,r,i)
-              boundary.break()
-      has
+    var symbolList = List[Symbol]()
+    var hasSymbol = false
+    def hasSurround(
+        lines: Seq[String],
+        totalSymbols: ArrayBuffer[Symbol] = ArrayBuffer[Symbol]()
+    ): Boolean =
+      val fromRow = max(row - 1, 0)
+      val toRow = min(row + 1, lines.length - 1)
+      val fromIndex = max(startIndex - 1, 0)
+      val toIndex = min(lines(0).length - 1, endIndex + 1)
+      for r <- fromRow to toRow; i <- fromIndex to toIndex do
+        lines(r)(i) match
+          case c if c.isDigit => {}
+          case c if c == '.'  => {}
+          case c: Char =>
+            hasSymbol = true
+            val symbol = Symbol(c, r, i)
+            if totalSymbols.contains(symbol) then
+              val s = totalSymbols.find(_ == symbol).get
+              s.Numbers = s.Numbers :+ this
+            else
+              symbol.Numbers = symbol.Numbers :+ this
+              totalSymbols += symbol
+      hasSymbol
     override def toString(): String =
-      s"- $value, row: $row, col: $startIndex to $endIndex, symbol: $symbol"
+      s"- $value, row: $row, col: $startIndex to $endIndex, hasSymbol: $hasSymbol"
   }
+
   val input = Source.fromFile("./Day03/a03").getLines().toVector
+  val testInput = Source.fromFile("./Day03/a03test").getLines().toVector
   def getNumbers(line: String, row: Int): List[Number] =
     var retList = List[Number]()
     var current = ""
@@ -42,7 +58,7 @@ object Star05:
           current += d
           if i == line.length - 1 then
             retList =
-              retList :+ Number(current.toInt, row, i - current.length+1, i)
+              retList :+ Number(current.toInt, row, i - current.length + 1, i)
             current = ""
         case _ if current != "" =>
           retList =
@@ -55,5 +71,16 @@ object Star05:
     println(listNumbers.filter(_.hasSurround(input)).foldLeft(0)(_ + _.value))
 
 object Star06:
-  val listNumbers: Vector[Star05.Number] = Star05.input.zipWithIndex.flatMap((s, r) => Star05.getNumbers(s, r))
-  listNumbers.filter(_.symbol.symbol == '*')
+  import Star05.{Number, Symbol, getNumbers}
+  def getGears(input: Vector[String]) =
+    var totalSymbols = ArrayBuffer[Symbol]()
+    val listNumbers = input.zipWithIndex.flatMap((s, r) => getNumbers(s, r))
+    listNumbers.filter(_.hasSurround(input, totalSymbols))
+    totalSymbols
+      .filter(_.symbol == '*')
+      .filter(_.Numbers.length == 2)
+      .map(_.Numbers.map(_.value).product)
+      .sum
+
+  @main def solve06 =
+    println(getGears(Star05.input))
--- README.md
+++ README.md
@@ -6,8 +6,8 @@ Join the advent of code at https://adventofcode.com/
 
 ## Current State
 
-| Day | First Puzzle | Second Puzzzle |
-| --: | :----------: | :------------: |
+|             Day             | First Puzzle | Second Puzzzle |
+| :-------------------------: | :----------: | :------------: |
 | [01](/Day01/advent01.scala) |      ⭐      |       ⭐       |
 | [02](/Day02/advent02.scala) |      ⭐      |       ⭐       |
-| [03](/Day03/advent03.scala) |      ⭐      |       ⏳       |
+| [03](/Day03/advent03.scala) |      ⭐      |       ⭐       |
